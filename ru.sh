9eb1067b7ae563361a97201ed8b62d6c450d2cb8
kedjour badr eddine <b.kedjour@outlook.com>
2017-08-17 19:10:23 +0200

    forcing images recreating

--- ru.sh
+++ ru.sh
@@ -1,4 +1,4 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 export BACKEND_PROD_EXTERNAL_DNS_NAME_OR_IP=$(curl ipinfo.io/ip)
-docker-compose up -d
+docker-compose up -d --force-recreate
